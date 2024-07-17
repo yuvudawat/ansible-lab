a33700ba7dfb8926d835c53b3cbfb8c8ddb60631
yuvudawat <98381529+yuvudawat@users.noreply.github.com>
2024-07-18 01:01:41 +0530

    Update kuberneters.c

--- kuberneters.c
+++ kuberneters.c
@@ -1,1 +1,13 @@
-
+sudo yum update -y
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+kubectl version
